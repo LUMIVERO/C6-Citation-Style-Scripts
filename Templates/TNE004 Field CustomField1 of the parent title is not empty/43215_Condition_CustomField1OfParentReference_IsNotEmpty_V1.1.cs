c75d291305e58b8970527eb195fffa0d6f683d01
frapemoy <frape@posteo.ch>
2018-06-21 17:28:12 +0200

    Initial commit

---
+++ Templates/TNE004 Field CustomField1 of the parent title is not empty/43215_Condition_CustomField1OfParentReference_IsNotEmpty_V1.1.cs
@@ -0,0 +1,30 @@
+﻿//#43215
+//Version 1.1
+//Feld CustomField1 des übergeordneten Titels ist nicht leer
+
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+		
+			if (citation == null) return false;
+			if (citation.Reference == null) return false;
+			if (citation.Reference.ParentReference == null) return false;
+			
+			var CustomField1 = citation.Reference.ParentReference.CustomField1;
+			
+			return !string.IsNullOrEmpty(CustomField1);
+			
+		}
+	}
+}
---
+++ Templates/TXY003 Field Author contains more than 4 authors/43232_Condition_Authors_ContainsMoreThanFourAuthors_C5_V1.0.cs
@@ -0,0 +1,30 @@
+//Citavi 5
+//#43232
+//Version 1.0
+
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+			//More than 4 authors, editors, organizations
+			if (citation == null) return false;
+			if (citation.Reference == null) return false;
+			
+			var authors = citation.Reference.AuthorsOrEditorsOrOrganizations;
+			if (authors == null || authors.Count == 0) return false;
+			
+			
+			return authors.Count() > 4;
+		}
+	}
+}
