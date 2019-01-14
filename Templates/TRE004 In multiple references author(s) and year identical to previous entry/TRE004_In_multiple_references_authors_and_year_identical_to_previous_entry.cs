5682c2df7ea57cd392ee8d4bdffc36b07b61d4bf
citavi-support <frape@posteo.ch>
2019-01-14 14:03:51 +0100

    New script

---
+++ Templates/TRE004 In multiple references author(s) and year identical to previous entry/TRE004_In_multiple_references_authors_and_year_identical_to_previous_entry.cs
@@ -0,0 +1,51 @@
+//TRE004
+//In multiple references author(s) and year identical to previous entry 
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+public class CustomTemplateCondition
+:
+ITemplateConditionMacro
+{
+public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+{
+if (citation == null) return false;
+if (citation.Reference == null) return false;
+
+//citation must be part of multiple citation
+PlaceholderCitation placeholderCitation = citation as PlaceholderCitation;
+if (placeholderCitation == null) return false;
+if (!placeholderCitation.IsPartOfMultipleCitation) return false;
+
+//citation must not be the first citation in a multiple citation and must have a predecessor
+if (placeholderCitation.IsFirstInMultipleCitation) return false;
+PlaceholderCitation previousPlaceholderCitation = placeholderCitation.PreviousPlaceholderCitation;
+if (previousPlaceholderCitation == null) return false;
+
+
+Reference thisReference = placeholderCitation.Reference;
+Reference previousReference = previousPlaceholderCitation.Reference;
+if (previousReference == null) return false;
+
+//reference must have the same year as its predecessor
+string thisYear = thisReference.YearResolved;
+string previousYear = previousReference.YearResolved;
+if (!thisYear.Equals(previousYear)) return false;
+
+//reference must have the same authors as its predecessor
+IEnumerable<Person> thisAuthors = thisReference.AuthorsOrEditorsOrOrganizations.ToList();
+IEnumerable<Person> previousAuthors = previousReference.AuthorsOrEditorsOrOrganizations.ToList();
+if (!thisAuthors.SequenceEqual(previousAuthors)) return false;
+
+return true;
+}
+}
+}
---
+++ Templates/TRE004 In multiple references author(s) and year identical to previous entry/readme.de.md
@@ -0,0 +1,13 @@
+# Übergeordnetes Werk oder Beitrag daraus wurde vorher einmal zitiert
+
+
+## Voraussetzungen
+Citavi 5 (oder höher)
+
+
+## Installation
+Siehe Citavi Handbuch: [Creating Custom Templates](http://www.citavi.com/creating_custom_templates)
+
+## Autor
+
+* **Jörg Pasch** [joepasch](https://github.com/joepasch)
