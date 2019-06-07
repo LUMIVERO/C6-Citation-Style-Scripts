32f906feaaad20972d48e6a11734400400507d31
Peter Meurer <31315727+PeterMeurer@users.noreply.github.com>
2019-06-07 12:24:06 +0200

    Add files via upload

---
+++ Templates/TRE006 Reference has been cited only once/TRE006_Reference_cited_only_once.cs
@@ -0,0 +1,29 @@
+//TRE006
+//Reference has been cited only once
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+			//Titel ist nur einmal in der FN zitiert
+			if (citation == null) return false;
+			if (citation.Reference == null) return false;
+			
+			FootnoteCitation footnoteCitation = citation as FootnoteCitation;
+			if (footnoteCitation == null) return false;
+			
+			return footnoteCitation.IsUniqueFootnote;
+			
+		}
+	}
+}
