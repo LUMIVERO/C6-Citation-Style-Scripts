9a41db48066608c3d857b43d6f0acac0ac742aa3
Susanne-S <31404555+Susanne-S@users.noreply.github.com>
2019-06-19 20:29:30 +0200

    Update CDA005_Date_Output_In_Different_Culture_Format.cs

--- Components/CDA Date/CDA005 Display name of month independent of style language/CDA005_Date_Output_In_Different_Culture_Format.cs
+++ Components/CDA Date/CDA005 Display name of month independent of style language/CDA005_Date_Output_In_Different_Culture_Format.cs
@@ -1,4 +1,8 @@
 //CDA005
+//Description: Output date field (including name of month) in different culture format depending on language of reference
+//Version: 1.2 Lines 107ff language specific output
+//Version: 1.1 If field has year information only, without month and day, only the year will be output here (instead of January, 1st of that year)
+
 
 using System;
 using System.Linq;
@@ -17,30 +21,59 @@ namespace SwissAcademic.Citavi.Citations
 	{
 		public IEnumerable<ITextUnit> GetTextUnits(ComponentPart componentPart, Template template, Citation citation, out bool handled)
 		{
-			//V1.1  If field has year information only, without month and day, only the year will be output here (instead of January, 1st of that year)
-
-
-			//CultureInfo targetCulture = new CultureInfo("fr");								//"neutrale" Kultur
-			CultureInfo targetCulture = CultureInfo.CreateSpecificCulture("en-US");				//spezifische Kultur
-
-
-
 			handled = false;
 
 			if (citation == null) return null;
 			if (citation.Reference == null) return null;
 			if (componentPart == null) return null;
 			if (componentPart.Elements == null || componentPart.Elements.Count == 0) return null;
+			
+			#region Find field elements of type DateTime
 
 			IEnumerable<DateTimeFieldElement> dateTimeFieldElements = componentPart.Elements.OfType<DateTimeFieldElement>();
 			if (dateTimeFieldElements == null || dateTimeFieldElements.Count() != 1) return null;
 
 			DateTimeFieldElement dateTimeFieldElement = dateTimeFieldElements.ElementAt(0);
-
 			if (dateTimeFieldElement == null) return null;
+			
+			#endregion
+			
+			#region Determine reference to look at
+			
+			Reference reference;
+			if (componentPart.Scope == ComponentPartScope.ParentReference)
+			{
+				if (citation.Reference.ParentReference == null) return null;
+				reference = citation.Reference.ParentReference as Reference;
+			}
+			else
+			{
+				reference = citation.Reference as Reference;
+			}
+			if (reference == null) return null;			
+			
+			#endregion Determine reference to look at
+			
+			#region Determine reference language
+			
+			Language language;
+			if (String.Equals(reference.LanguageCode, CultureInfo.GetCultureInfo("en").TwoLetterISOLanguageName, StringComparison.OrdinalIgnoreCase))
+			{
+				language = Language.English;
+			}
+			else if (String.Equals(reference.LanguageCode, CultureInfo.GetCultureInfo("de").TwoLetterISOLanguageName, StringComparison.OrdinalIgnoreCase))
+			{
+				language = Language.German;
+			}
+			else
+			{
+				language = Language.Other;
+			}
+			
+			#endregion Determine reference language
 
 			var propertyId = dateTimeFieldElement.PropertyId;
-			var dateTimeStringValue = citation.Reference.GetValue(propertyId) as string;
+			var dateTimeStringValue = reference.GetValue(propertyId) as string;
 			if (string.IsNullOrEmpty(dateTimeStringValue)) return null;
 
 
@@ -52,7 +85,8 @@ namespace SwissAcademic.Citavi.Citations
 
 			List<LiteralElement> literalElements = new List<LiteralElement>();
 
-			int counter = 1;
+			TextUnitCollection debug = new TextUnitCollection();
+			//int counter = 1;
 			foreach (Segment segment in segments)
 			{
 				switch (segment.type)
@@ -68,14 +102,94 @@ namespace SwissAcademic.Citavi.Citations
 					case SegmentType.DateTime:
 						{
 							string newDateString;
+							
+							#region YEAR information only
+							
 							if (!segment.ContainsMonthInformation && !segment.ContainsDayInformation)
 							{
-								newDateString = segment.dateTime.ToString("yyyy", targetCulture);
+								switch (language)
+								{
+									default:
+									case (Language.English):
+									{
+										newDateString = segment.dateTime.ToString("yyyy", new CultureInfo("en-US")); //or "en-UK"
+									}
+									break;
+									
+									case (Language.German):
+									{
+										newDateString = segment.dateTime.ToString("yyyy", new CultureInfo("de-DE")); // or "de-CH" or "de-AT"
+									}
+									break;
+									
+									case (Language.Other):
+									{
+										newDateString = segment.dateTime.ToString("yyyy", new CultureInfo("en-US"));
+									}
+									break;	
+								}
 							}
+							
+							#endregion
+							
+							#region YEAR and MONTH
+							
+							else if (!segment.ContainsDayInformation)
+							{
+								switch (language)
+								{
+									default:
+									case (Language.English):
+									{
+										newDateString = segment.dateTime.ToString("MM/yyyy", new CultureInfo("en-US")); //or "en-UK"
+									}
+									break;
+									
+									case (Language.German):
+									{
+										newDateString = segment.dateTime.ToString("MMM yyyy", new CultureInfo("de-DE")); // or "de-CH" or "de-AT"
+									}
+									break;
+									
+									case (Language.Other):
+									{
+										newDateString = segment.dateTime.ToString("MM/yyyy", new CultureInfo("en-US"));
+									}
+									break;	
+								}
+							}
+							
+							#endregion
+							
+							#region YEAR and MONTH and DAY
+							
 							else
 							{
-								newDateString = segment.dateTime.ToString(dateTimeFieldElement.Format, targetCulture);
+								switch (language)
+								{
+									default:
+									case (Language.English):
+									{
+										newDateString = segment.dateTime.ToString("d", new CultureInfo("en-US")); //or "en-UK"
+									}
+									break;
+									
+									case (Language.German):
+									{
+										newDateString = segment.dateTime.ToString("dd. MM yyyy", new CultureInfo("de-DE")); // or "de-CH" or "de-AT"
+									}
+									break;
+									
+									case (Language.Other):
+									{
+										newDateString = segment.dateTime.ToString("MM/dd/yyyy", new CultureInfo("en-US"));
+									}
+									break;	
+								}
 							}
+							
+							#endregion
+							
 							var literalElement = new LiteralElement(componentPart, newDateString);
 							literalElement.FontStyle = dateTimeFieldElement.FontStyle;
 							literalElements.Add(literalElement);
@@ -95,7 +209,12 @@ namespace SwissAcademic.Citavi.Citations
 			return null;
 		}
 
-
+		private enum Language
+		{
+			English,
+			German,
+			Other
+		}
 
 		private struct Segment
 		{
@@ -157,8 +276,5 @@ namespace SwissAcademic.Citavi.Citations
 
 			return segments;
 		}
-
-
-
 	}
 }
