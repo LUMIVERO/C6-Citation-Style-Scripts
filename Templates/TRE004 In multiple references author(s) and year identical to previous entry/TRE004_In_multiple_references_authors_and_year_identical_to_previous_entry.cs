8e0f040fda4747e669b04e2845076f969b6d602a
Susanne-S <31404555+Susanne-S@users.noreply.github.com>
2020-01-20 14:48:59 +0100

    Update TRE004_In_multiple_references_authors_and_year_identical_to_previous_entry.cs

--- Templates/TRE004 In multiple references author(s) and year identical to previous entry/TRE004_In_multiple_references_authors_and_year_identical_to_previous_entry.cs
+++ Templates/TRE004 In multiple references author(s) and year identical to previous entry/TRE004_In_multiple_references_authors_and_year_identical_to_previous_entry.cs
@@ -1,7 +1,6 @@
 //TRE004
 //In multiple references author(s) and year identical to previous entry 
 
-
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -11,41 +10,41 @@ using SwissAcademic.Collections;
 
 namespace SwissAcademic.Citavi.Citations
 {
-public class CustomTemplateCondition
-:
-ITemplateConditionMacro
-{
-public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
-{
-if (citation == null) return false;
-if (citation.Reference == null) return false;
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+			if (citation == null) return false;
+			if (citation.Reference == null) return false;
 
-//citation must be part of multiple citation
-PlaceholderCitation placeholderCitation = citation as PlaceholderCitation;
-if (placeholderCitation == null) return false;
-if (!placeholderCitation.IsPartOfMultipleCitation) return false;
+			//citation must be part of multiple citation
+			PlaceholderCitation placeholderCitation = citation as PlaceholderCitation;
+			if (placeholderCitation == null) return false;
+			if (!placeholderCitation.IsPartOfMultipleCitation) return false;
 
-//citation must not be the first citation in a multiple citation and must have a predecessor
-if (placeholderCitation.IsFirstInMultipleCitation) return false;
-PlaceholderCitation previousPlaceholderCitation = placeholderCitation.PreviousPlaceholderCitation;
-if (previousPlaceholderCitation == null) return false;
+			//citation must not be the first citation in a multiple citation and must have a predecessor
+			if (placeholderCitation.IsFirstInMultipleCitation) return false;
+			PlaceholderCitation previousPlaceholderCitation = placeholderCitation.PreviousPlaceholderCitation;
+			if (previousPlaceholderCitation == null) return false;
 
 
-Reference thisReference = placeholderCitation.Reference;
-Reference previousReference = previousPlaceholderCitation.Reference;
-if (previousReference == null) return false;
+			Reference thisReference = placeholderCitation.Reference;
+			Reference previousReference = previousPlaceholderCitation.Reference;
+			if (previousReference == null) return false;
 
-//reference must have the same year as its predecessor
-string thisYear = thisReference.YearResolved;
-string previousYear = previousReference.YearResolved;
-if (!thisYear.Equals(previousYear)) return false;
+			//reference must have the same year as its predecessor
+			string thisYear = thisReference.YearResolved;
+			string previousYear = previousReference.YearResolved;
+			if (!thisYear.Equals(previousYear)) return false;
 
-//reference must have the same authors as its predecessor
-IEnumerable<Person> thisAuthors = thisReference.AuthorsOrEditorsOrOrganizations.ToList();
-IEnumerable<Person> previousAuthors = previousReference.AuthorsOrEditorsOrOrganizations.ToList();
-if (!thisAuthors.SequenceEqual(previousAuthors)) return false;
+			//reference must have the same authors as its predecessor
+			IEnumerable<Person> thisAuthors = thisReference.AuthorsOrEditorsOrOrganizations.ToList();
+			IEnumerable<Person> previousAuthors = previousReference.AuthorsOrEditorsOrOrganizations.ToList();
+			if (!thisAuthors.SequenceEqual(previousAuthors)) return false;
 
-return true;
-}
-}
+			return true;
+		}
+	}
 }
