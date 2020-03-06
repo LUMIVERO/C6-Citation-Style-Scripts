18c3b99c5223507c4c2813de43da67dea9b35959
Peter Meurer <31315727+PeterMeurer@users.noreply.github.com>
2020-03-06 17:29:24 +0100

    Create TRE010_Other_work_in_the_same_series_was_cited_in_a_previous_footnote.cs

---
+++ Templates/TRE010 Other work in the same series was cited in a previous footnote/TRE010_Other_work_in_the_same_series_was_cited_in_a_previous_footnote.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+			//Other work in the same series was cited in a previous footnote
+            if (citation == null) return false;
+
+            var currentFootnoteCitation = citation as FootnoteCitation;
+            if (currentFootnoteCitation == null) return false;
+            
+			Reference currentReference = currentFootnoteCitation.Reference;
+			SeriesTitle currentSeries = currentReference.SeriesTitle;
+			if (currentSeries == null && currentReference.ParentReference != null)
+			{
+				currentSeries = currentReference.ParentReference.SeriesTitle;
+			}
+			if (currentSeries == null) return false;
+			
+			
+            // regarding placeholderCitation.PersonOnly: see comment for RepeatingInTextCitation
+            if (currentFootnoteCitation.RuleSetOverride != RuleSetOverride.None) return false;
+            if (currentFootnoteCitation.YearOnly) return false;
+
+            foreach (var otherFootnoteCitation in currentFootnoteCitation.CitationManager.FootnoteCitations)
+            {
+                if (otherFootnoteCitation == currentFootnoteCitation) break; //we just look in the direction of the beginning of the document
+				
+                Reference otherReference = otherFootnoteCitation.Reference;
+				if (otherReference == null) continue;
+				
+				SeriesTitle otherSeries = otherReference.SeriesTitle;
+				if (otherSeries == null && otherReference.ParentReference != null)
+				{
+					otherSeries = otherReference.ParentReference.SeriesTitle;
+				}
+				if (otherSeries == null) continue;				
+
+                if (otherSeries == currentSeries && otherReference != currentReference)
+				{
+					return true;
+				}
+            }
+
+			//still here? nothing found, return false
+            return false;
+		}
+	}
+}
