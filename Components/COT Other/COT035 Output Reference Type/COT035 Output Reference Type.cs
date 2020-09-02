7d2ab3745fa5f3401569fe0417cc84e5ec9a5e02
Susanne-S <31404555+Susanne-S@users.noreply.github.com>
2020-09-02 16:15:53 +0200

    Create COT035 Output Reference Type.cs

---
+++ Components/COT Other/COT035 Output Reference Type/COT035 Output Reference Type.cs
@@ -0,0 +1,39 @@
+//COT035 Output Reference Type
+
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+using SwissAcademic.Drawing;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class ComponentPartFilter
+		:
+		IComponentPartFilter
+	{
+		public IEnumerable<ITextUnit> GetTextUnits(ComponentPart componentPart, Template template, Citation citation, out bool handled)
+		{
+			//return handled = true if this macro generates the output (as an IEnumerable<ITextUnit>); the standard output will be suppressed
+			//return handled = false if you want Citavi to produce the standard output;
+
+			handled = true;
+
+			if (citation == null) return null;
+			if (citation.Reference == null) return null;
+
+			TextUnitCollection output = new TextUnitCollection();
+			LiteralTextUnit text;
+
+			string referenceType = citation.Reference.ReferenceType.ToString();
+
+			text = new LiteralTextUnit(referenceType, FontStyle.Neutral);
+		//	text = new LiteralTextUnit(referenceType, FontStyle.Bold | FontStyle.SmallCaps);
+
+			output.Add(text);
+
+			return output;
+		}
+	}
+}
