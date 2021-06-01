8b0a4ccfd8d00a8c3fa7aa484fbf6e4262b88e66
Susanne-S <31404555+Susanne-S@users.noreply.github.com>
2021-06-01 17:30:32 +0200

    Create TRE015 Consecutive entry in the bibliography - same reference title.cs

---
+++ Templates/TRE015 Consecutive entry in the bibliography - same reference title/TRE015 Consecutive entry in the bibliography - same reference title.cs
@@ -0,0 +1,74 @@
+//TRE015 - Consecutive entry in the bibliography - same reference title
+//Description:	Previous bibliography citation has the same title as THIS bibliography citation
+//Version 1.0
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class CustomTemplateCondition
+		:
+		ITemplateConditionMacro
+	{
+		public bool IsTemplateForReference(ConditionalTemplate template, Citation citation)
+		{
+			if (citation == null) return false;
+			if (citation.CitationManager == null) return false;
+			if (citation.Reference == null) return false;
+			if (citation.Reference.Title == null) return false;
+			
+			//filter deactivates itself, if the bibliography is NOT YET completely sorted
+			//this is necessary to avoid that this filter in turn changes the sort order, that it depends upon
+			if (citation.CitationManager.BibliographyCitations.IsSorted == false) return false;
+
+			#region ThisBibliographyCitation
+
+			var thisBibliographyCitation = citation as BibliographyCitation;
+			if (thisBibliographyCitation == null) return false;
+
+			#endregion ThisBibliographyCitation
+
+			#region PreviousBibliographyCitation
+
+			var previousBibliographyCitation = GetPreviousVisibleBibliographyCitation(thisBibliographyCitation);
+			if (previousBibliographyCitation == null) return false;
+			if (previousBibliographyCitation.Reference == null) return false;
+			if (previousBibliographyCitation.NoBib == true) return false;
+
+			#endregion PreviousBibliographyCitation
+
+			if (previousBibliographyCitation == thisBibliographyCitation) return false;
+			if (previousBibliographyCitation.Reference.Title == null) return false;
+			if (previousBibliographyCitation.Reference.Title.Equals(thisBibliographyCitation.Reference.Title)) return true;
+
+			//still here
+			return false;
+		}
+		
+		#region GetPreviousVisibleCitation
+
+		private static BibliographyCitation GetPreviousVisibleBibliographyCitation(BibliographyCitation bibliographyCitation)
+		{
+			if (bibliographyCitation == null) return null;
+			BibliographyCitation previousBibliographyCitation = bibliographyCitation;
+
+			//consider nobib
+			do
+			{
+				previousBibliographyCitation = previousBibliographyCitation.PreviousBibliographyCitation;
+				if (previousBibliographyCitation == null) return null;
+
+			} while (previousBibliographyCitation.NoBib == true);
+
+			//still here? found one!
+			return previousBibliographyCitation;
+		}
+
+		#endregion GetPreviousCitation
+	}
+}
