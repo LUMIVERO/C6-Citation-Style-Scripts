1e3b56e17cce459e591d72e62bd05f4e60d4432b
Sebastian Pabel <sebastian.pabel@citavi.com>
2023-12-27 14:39:49 +0100

    Added COT042 - Volume as Roman Numeral
    New script added to display volume number als lower case roman numeral

---
+++ Components/COT Other/COT042 Display Volume as Roman Numeral/COT042 Display Volume as Roman Numeral.cs
@@ -0,0 +1,39 @@
+//CDA003
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Collections;
+using SwissAcademic.Drawing;
+
+namespace SwissAcademic.Citavi.Citations
+{
+	public class ComponentPartFilter
+		:
+		IComponentPartFilter
+	{
+		public IEnumerable<ITextUnit> GetTextUnits(ComponentPart componentPart, Template template, Citation citation, out bool handled)
+		{
+			handled = false;
+
+			if (componentPart == null) return null;
+			if (citation == null || citation.Reference == null) return null;
+
+			var volumeString = citation.Reference.Volume;
+			if (string.IsNullOrWhiteSpace(volumeString)) return null;
+
+			var output = new TextUnitCollection();
+			
+			var volumeStringRoman = NumeralSystemConverter.ToRomanNumber(arabicNumber: volumeString, lowerCase: true);
+			
+			var volumeStringTextUnit = new LiteralTextUnit(volumeStringRoman);
+			output.Add(volumeStringTextUnit);
+
+			handled = true;
+			return output;
+
+		}
+	}
+}
---
+++ Components/COT Other/COT042 Display Volume as Roman Numeral/readme.de.md
@@ -0,0 +1,16 @@
+# Leerzeichen im Reihentitel durch geschützte Leerzeichen ersetzen
+Dieses Skript gibt das Feld Jahrgang (Volume) als römische Zahl aus.
+
+## Voraussetzungen
+Citavi 6 (oder höher)
+
+## Anwendung
+Das Skript kann im Zitationsstileditor an eine der folgenden Komponente angehängt werden:
+Jahrgang (Volume)
+
+## Installation
+Siehe Citavi Handbuch: [Using Programmable Components](https://www.citavi.com/programmable_components)
+
+## Autor
+
+* Sebastian Pabel
